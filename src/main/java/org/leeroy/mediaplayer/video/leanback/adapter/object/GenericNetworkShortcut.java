711a4c1755c824fb9d9ffba999339cc5a5c033d1
Leeroy <leeroy@nowhere.com>
2025-09-01 17:01:34 +1000

    Rebrand to LeeroyFlix
    New Icon for Phone Version.
    
    TV Banner, Remove FAQ, Remove Non-English Translations
    
    Rebrand README.md
    
    Full LeeroyFlix rebrand in Source Code
    
    Main package name change
    
    Fix Database Fields
    
    Default prefs changes
    
    UI Changes
    
    Remove vX.X.X from Main Phone UI
    
    Change Library to Media Library
    
    Add comments to code
    
    change mediacentre to mediaplayer
    
    Media Libary backup option string changes
    
    Change opensubtitles OpenSubtitles
    
    Fix Rebrand Source Folders, Class not correct.
    
    Re-brand rename MainActivity etc.

---
+++ src/main/java/org/leeroy/mediaplayer/video/leanback/adapter/object/GenericNetworkShortcut.java
@@ -0,0 +1,46 @@
+// Copyright 2022 Courville Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.leeroy.mediaplayer.video.leanback.adapter.object;
+
+import android.net.Uri;
+
+import org.leeroy.filecorelibrary.FileUtils;
+import org.leeroy.filecorelibrary.contentstorage.ContentStorageFileEditor;
+import org.leeroy.filecorelibrary.ftp.FtpFileEditor;
+import org.leeroy.filecorelibrary.jcifs.JcifsFileEditor;
+import org.leeroy.filecorelibrary.localstorage.LocalStorageFileEditor;
+import org.leeroy.filecorelibrary.sftp.SftpFileEditor;
+import org.leeroy.filecorelibrary.zip.ZipFileEditor;
+import org.leeroy.mediaplayer.video.R;
+import org.leeroy.mediaplayer.video.utils.VideoUtils;
+
+import java.io.Serializable;
+
+public class GenericNetworkShortcut extends Shortcut implements Serializable {
+
+    public GenericNetworkShortcut(long id, String fullPath, String name, String friendlyUri) {
+        super(id, fullPath, friendlyUri, name);
+    }
+
+    public Uri getUri() {
+        return Uri.parse(mFullPath);
+    }
+
+    @Override
+    public int getImage() {
+        Uri uri = getUri();
+        return VideoUtils.getShortcutImageLeanback(uri);
+    }
+}
---
+++ src/main/java/org/leeroy/mediaplayer/video/leanback/tvshow/EpisodesByDateFragment.java
@@ -0,0 +1,128 @@
+// Copyright 2017 LeeroyFlix
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.leeroy.mediaplayer.video.leanback.tvshow;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.ContextCompat;
+import androidx.leanback.widget.SearchOrbView;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
+import androidx.preference.PreferenceManager;
+
+import org.leeroy.mediaplayer.video.R;
+import org.leeroy.mediaplayer.video.browser.loader.EpisodesByDateLoader;
+import org.leeroy.mediaplayer.video.browser.loader.EpisodesNoAnimeByDateLoader;
+import org.leeroy.mediaplayer.video.browser.loader.EpisodesSelectionLoader;
+import org.leeroy.mediaplayer.video.leanback.VideosByFragment;
+import org.leeroy.mediaplayer.video.utils.VideoPreferencesCommon;
+import org.leeroy.mediaprovider.video.VideoStore;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EpisodesByDateFragment extends VideosByFragment {
+
+    private static final Logger log = LoggerFactory.getLogger(EpisodesByDateFragment.class);
+
+    private static final String SORT_PARAM_KEY = EpisodesByDateFragment.class.getName() + "_SORT";
+    private static final String VIEW_PARAM_KEY = EpisodesByDateFragment.class.getName() + "_VIEW";
+
+    private SharedPreferences mPrefs;
+    private int mDateView;
+    private boolean mSeparateAnimeFromShowMovie;
+
+    public EpisodesByDateFragment() {
+        super(VideoStore.Video.VideoColumns.SCRAPER_E_AIRED);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        log.debug("onActivityCreated");
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        mDateView = mPrefs.getInt(VIEW_PARAM_KEY, 0);
+        mSeparateAnimeFromShowMovie = PreferenceManager.getDefaultSharedPreferences(getActivity()).getBoolean(VideoPreferencesCommon.KEY_SEPARATE_ANIME_MOVIE_SHOW, VideoPreferencesCommon.SEPARATE_ANIME_MOVIE_SHOW_DEFAULT);
+
+        super.onActivityCreated(savedInstanceState);
+
+        setTitle(getString(R.string.episodes_by_date));
+        SearchOrbView searchOrbView = (SearchOrbView) getView().findViewById(R.id.title_orb);
+        searchOrbView.setOrbIcon(ContextCompat.getDrawable(getActivity(), R.drawable.orb_list));
+        setOnSearchClickedListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String[] names = { getString(R.string.date_view_week), getString(R.string.date_view_month), getString(R.string.date_view_year) };
+
+                new AlertDialog.Builder(getActivity())
+                        .setSingleChoiceItems(names, mDateView, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                if (mDateView != which) {
+                                    mDateView = which;
+                                    // Save the view mode
+                                    mPrefs.edit().putInt(VIEW_PARAM_KEY, mDateView).commit();
+                                    LoaderManager.getInstance(EpisodesByDateFragment.this).restartLoader(-1, null, EpisodesByDateFragment.this);
+                                }
+                                dialog.dismiss();
+                            }
+                        })
+                        .create().show();
+            }
+        });
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        log.debug("onCreateLoader id=", id);
+        if (id == -1) {
+            // List of categories
+            return getSubsetLoader(getActivity());
+        } else {
+            // One of the row
+            return new EpisodesSelectionLoader(getActivity(), args.getString("ids"), args.getString("sort"));
+        }
+    }
+
+    @Override
+    protected Loader<Cursor> getSubsetLoader(Context context) {
+        if (mSeparateAnimeFromShowMovie) return new EpisodesNoAnimeByDateLoader(context, EpisodesNoAnimeByDateLoader.DateView.values()[mDateView]);
+        else return new EpisodesByDateLoader(context, EpisodesByDateLoader.DateView.values()[mDateView]);
+    }
+
+    @Override
+    protected CharSequence[] getSortOrderEntries() {
+        return null;
+    }
+
+    @Override
+    protected String item2SortOrder(int item) {
+        return "";
+    }
+
+    @Override
+    protected int sortOrder2Item(String sortOrder) {
+        return 0;
+    }
+
+    @Override
+    protected String getSortOrderParamKey() {
+        return SORT_PARAM_KEY;
+    }
+
+}
