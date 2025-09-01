c6f885bf5f0db33057710bb7d94d47b7aa4d37d9
Leeroy <leeroy@nowhere.com>
2025-09-01 17:01:34 +1000

    Rebrand to LeeroyFlix
    New Icon for Phone Version.
    
    TV Banner, Remove FAQ, Remove Non-English Translations
    
    Rebrand README.md
    
    Full LeeroyFlix rebrand in Source Code
    
    Main package name change
    
    Fix Database Fields
    
    Default prefs changes
    
    UI Changes
    
    Remove vX.X.X from Main Phone UI
    
    Change Library to Media Library
    
    Add comments to code
    
    change mediacentre to mediaplayer
    
    Media Libary backup option string changes
    
    Change opensubtitles OpenSubtitles
    
    Fix Rebrand Source Folders, Class not correct.
    
    Re-brand rename MainActivity etc.
    
    Save/Restore Brightness Setting
    * Removed new drawable assignment on Brightness UP
    * mBrightness is saved on Gesture for Sync of Slider
    
    Add Remember Brghtness Pref to Brightness Dialog
    *Put Brightness Checkbox in Centre
    *Set enabled the same as Slider.

---
+++ src/main/java/org/leeroy/mediaplayer/video/leanback/VideosByListActivity.java
@@ -0,0 +1,14 @@
+package org.leeroy.mediaplayer.video.leanback;
+
+import androidx.fragment.app.Fragment;
+
+public class VideosByListActivity extends SingleFragmentActivity {
+    @Override
+    public Fragment getFragmentInstance() {
+        return new VideosByListFragment();
+    }
+
+    public void onBackPressed(){
+        finish();
+    }
+}
