bab5cd4c3b8c504947efb154bf8fcebc72e2216b
Leeroy <leeroy@nowhere.com>
2025-09-01 17:01:34 +1000

    Rebrand to LeeroyFlix
    New Icon for Phone Version.
    
    TV Banner, Remove FAQ, Remove Non-English Translations
    
    Rebrand README.md
    
    Full LeeroyFlix rebrand in Source Code
    
    Main package name change
    
    Fix Database Fields
    
    Default prefs changes
    
    UI Changes
    
    Remove vX.X.X from Main Phone UI
    
    Change Library to Media Library
    
    Add comments to code
    
    change mediacentre to mediaplayer
    
    Media Libary backup option string changes
    
    Change opensubtitles OpenSubtitles
    
    Fix Rebrand Source Folders, Class not correct.
    
    Re-brand rename MainActivity etc.

---
+++ src/main/java/org/leeroy/mediaplayer/video/browser/loader/UpNextLoader.java
@@ -0,0 +1,71 @@
+// Copyright 2025 Courville Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.leeroy.mediaplayer.video.browser.loader;
+
+import android.content.Context;
+
+import org.leeroy.mediaprovider.video.VideoStore;
+
+public class UpNextLoader extends VideoLoader {
+
+    public UpNextLoader(Context context) {
+        super(context);
+        init();
+        // cf. https://github.com/nova-video-player/aos-AVP/issues/134 reduce strain
+        // only updates the CursorLoader on data change every 10s since used only in LeeroyFlixFragment as nonScraped box presence
+        if (VideoLoader.ALLVIDEO_THROTTLE) setUpdateThrottle(VideoLoader.ALLVIDEO_THROTTLE_DELAY);
+    }
+
+    @Override
+    public String getSelection() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(super.getSelection());
+
+        if (builder.length() > 0)
+            builder.append(" AND ");
+
+        // Show ONLY "up next" episodes/movies - NO currently watching videos (bookmarks)
+        // /!\ note that it assumes that there is no more than 1000 episodes in a season
+
+        builder.append(
+                "(" +
+                // Part 1: Next episodes in TV series (excluding in-progress)
+                "(s_id IS NOT NULL AND " + VideoStore.Video.VideoColumns.LEEROYFLIX_TRAKT_SEEN + " = 0 AND lfx_hiddenbyuser = 0 AND (bookmark IS NULL OR bookmark <= 0) AND " +
+                "_id IN (SELECT _id FROM (SELECT v._id, ROW_NUMBER() OVER(PARTITION BY v.s_id ORDER BY v.e_season, v.e_episode) as rn FROM video v JOIN ( SELECT s_id, MAX(e_season * 1000 + e_episode) AS max_watched_episode FROM video WHERE " + VideoStore.Video.VideoColumns.LEEROYFLIX_TRAKT_SEEN + " = 1 AND lfx_hiddenbyuser = 0 GROUP BY s_id ) AS lw ON v.s_id = lw.s_id WHERE v." + VideoStore.Video.VideoColumns.LEEROYFLIX_TRAKT_SEEN + " = 0 AND v.lfx_hiddenbyuser = 0 AND (v.e_season * 1000 + v.e_episode) > lw.max_watched_episode ) WHERE rn = 1))" +
+                " OR " +
+                // Part 2: Next movies in collections (excluding in-progress)
+                "(m_coll_id IS NOT NULL AND " + VideoStore.Video.VideoColumns.LEEROYFLIX_TRAKT_SEEN + " = 0 AND lfx_hiddenbyuser = 0 AND (bookmark IS NULL OR bookmark <= 0) AND " +
+                "_id IN (SELECT _id FROM (SELECT v._id, ROW_NUMBER() OVER(PARTITION BY v.m_coll_id ORDER BY v.m_year) as rn FROM video v JOIN ( SELECT m_coll_id, MAX(m_year) AS max_watched_year FROM video WHERE " + VideoStore.Video.VideoColumns.LEEROYFLIX_TRAKT_SEEN + " = 1 AND lfx_hiddenbyuser = 0 GROUP BY m_coll_id ) AS lw ON v.m_coll_id = lw.m_coll_id WHERE v." + VideoStore.Video.VideoColumns.LEEROYFLIX_TRAKT_SEEN + " = 0 AND v.lfx_hiddenbyuser = 0 AND v.m_year > lw.max_watched_year ) WHERE rn = 1))" +
+                ")"
+        );
+
+        return builder.toString();
+    }
+
+    @Override
+    public String getSortOrder() {
+        // Sort by the play time of the *last watched* item in the series/collection.
+        return "COALESCE(" +
+                // For series: get the play time of the most recently *watched* episode
+                "(SELECT lw.lfx_lastTimePlayed FROM video lw WHERE lw.s_id = video.s_id AND lw." + VideoStore.Video.VideoColumns.LEEROYFLIX_TRAKT_SEEN + " = 1 ORDER BY lw.e_season DESC, lw.e_episode DESC LIMIT 1), " +
+                // For collections: get the play time of the most recently *watched* movie
+                "(SELECT lw.lfx_lastTimePlayed FROM video lw WHERE lw.m_coll_id = video.m_coll_id AND lw." + VideoStore.Video.VideoColumns.LEEROYFLIX_TRAKT_SEEN + " = 1 ORDER BY lw.m_year DESC LIMIT 1), " +
+                "0" + // Fallback value if no watched item is found
+            ") DESC, " +
+        // Add secondary sort for stability and logical ordering within the same play time
+        "s_name, e_season, e_episode, m_name, m_year" +
+        " LIMIT 100";
+    }
+}
---
+++ src/main/java/org/leeroy/mediaplayer/video/browser/loader/VideosSelectionLoader.java
@@ -0,0 +1,65 @@
+package org.leeroy.mediaplayer.video.browser.loader;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.loader.content.AsyncTaskLoader;
+
+import org.leeroy.mediaprovider.video.VideoStore;
+
+import java.lang.reflect.Field;
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by vapillon on 10/04/15.
+ */
+public class VideosSelectionLoader extends MoviesLoader {
+
+    private static final String TAG = "VideosSelectionLoader";
+
+    protected final String mListOfIds;
+    protected String mSortOrder;
+
+    public VideosSelectionLoader(Context context, String listOfMoviesIds) {
+        this(context, listOfMoviesIds, DEFAULT_SORT);
+    }
+
+    public VideosSelectionLoader(Context context, String listOfIds, String SortOrder) {
+        super(context, true);
+        // self introspection to use another Executor than AsyncTaskLoader which has 128 threads but a total queue of 10... cf. https://github.com/nova-video-player/aos-AVP/issues/141
+        if (VideoLoader.VIDEOSELECTION_CUSTOM_EXECUTOR) {
+            try {
+                Field f = AsyncTaskLoader.class.getDeclaredField("mExecutor");
+                f.setAccessible(true);
+                f.set(this, VideoLoader.videoSelectionLoaderExecutor);
+            } catch (NoSuchFieldException e) {
+                Log.w(TAG, "VideoLoader caught NoSuchFieldException ", e);
+            } catch (IllegalAccessException e) {
+                Log.w(TAG, "VideoLoader caught IllegalAccessException ", e);
+            }
+        }
+        if (VideoLoader.VIDEOSELECTION_THROTTLE) setUpdateThrottle(VideoLoader.VIDEOSELECTION_THROTTLE_DELAY);
+        mListOfIds = listOfIds;
+        mSortOrder = SortOrder;
+        init();
+    }
+
+    @Override
+    public String getSortOrder() {
+        return mSortOrder;
+    }
+
+    @Override
+    public String getSelection() {
+        return VideoStore.Video.VideoColumns._ID + " IN (" + mListOfIds + ")";
+    }
+
+    @Override
+    public String[] getSelectionArgs() {
+        return null;
+    }
+
+}
