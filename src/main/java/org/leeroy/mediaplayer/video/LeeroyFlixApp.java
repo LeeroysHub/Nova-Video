1d85711d3a990966ce626d6384299ee09def1151
Leeroy <leeroy@nowhere.com>
2025-10-26 12:32:05 +1100

    Turn debug flags OFF

--- src/main/java/org/leeroy/mediaplayer/video/LeeroyFlixApp.java
+++ src/main/java/org/leeroy/mediaplayer/video/LeeroyFlixApp.java
@@ -809,7 +809,7 @@ public class LeeroyFlixApp extends Application implements DefaultLifecycleObserv
         Security.insertProviderAt(new BouncyCastleProvider(), 1);
     }
 
-    static boolean DBG = true;
+    static boolean DBG = false;
 
     public void loadLocale() {
         // Warning no log.debug at this stage
