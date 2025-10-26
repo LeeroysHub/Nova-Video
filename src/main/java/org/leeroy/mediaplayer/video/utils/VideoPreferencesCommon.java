19d1fb06818e2a3cd6ce96090b06c793850a2f31
Leeroy <leeroy@nowhere.com>
2025-10-26 12:32:05 +1100

    Turn debug flags OFF

--- src/main/java/org/leeroy/mediaplayer/video/utils/VideoPreferencesCommon.java
+++ src/main/java/org/leeroy/mediaplayer/video/utils/VideoPreferencesCommon.java
@@ -1097,7 +1097,7 @@ public class VideoPreferencesCommon implements OnSharedPreferenceChangeListener 
         System.exit(0); // Optionally use this to ensure the app process is fully restarted.
     }
 
-    private static boolean DBG = true;
+    private static boolean DBG = false;
 
     private void rescanPath(String s) {
         isMediaScannerScanning(getActivity().getContentResolver());
