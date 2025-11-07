a438a0dfb265d13e60084e6916d60860731ca14e
Leeroy <leeroy@nowhere.com>
2025-11-07 17:46:03 +1100

    Audio Speed select down to 10% (AudioTrack only)

--- src/main/java/org/leeroy/mediaplayer/video/player/PlayerService.java
+++ src/main/java/org/leeroy/mediaplayer/video/player/PlayerService.java
@@ -462,6 +462,13 @@ public class PlayerService extends Service implements Player.Listener, IndexHelp
 
         // Reset explicit position at the start
         mExplicitPosition = -1;
+        
+        //Use the 0.5 to 2.0x time for ATEMPO, 0.10 to 2.0x for AudioTrack
+        if (mPreferences.getBoolean("audio_speed_audiotrack", false)) {
+            mAudioSpeedMin = 0.1f;
+        } else {
+            mAudioSpeedMin = 0.5f;
+        }
 
         // Check if floating player is passing position when switching between players
         if (intent.hasExtra("floating_player_position")) {
@@ -1804,16 +1811,15 @@ public class PlayerService extends Service implements Player.Listener, IndexHelp
 
     public void setAudioSpeed(float speed, boolean force) {
         boolean speedChanged = speed != mAudioSpeed || force;
-        if (speedChanged &&
-                (Integer.parseInt(mPreferences.getString("force_audio_passthrough_multiple","0")) == 0) &&
-                speed > 0.45f && speed < 2.05f) { // min granularity is 0.05
-            log.debug("setAudioSpeed: audio speed changed from {} to {}", mAudioSpeed, speed);
-            mAudioSpeed = speed;
-            if ((AUDIO_SPEED_ON_THE_FLY && mPreferences.getBoolean(KEY_PLAYBACK_SPEED,false)) || force) {
-                mPlayer.setAvSpeed(mAudioSpeed);
+        if (Integer.parseInt(mPreferences.getString("force_audio_passthrough_multiple","0")) == 0) {
+            if (speedChanged && (speed > (mAudioSpeedMin - 0.05f) && speed < (mAudioSpeedMax + 0.05f))) { // min granularity is 0.05
+                log.debug("setAudioSpeed: audio speed changed from {} to {}", mAudioSpeed, speed);
+                mAudioSpeed = speed;
+                if ((AUDIO_SPEED_ON_THE_FLY && mPreferences.getBoolean(KEY_PLAYBACK_SPEED,false)) || force) {
+                    mPlayer.setAvSpeed(mAudioSpeed);
+                }
             }
-        }
-        if (Integer.parseInt(mPreferences.getString("force_audio_passthrough_multiple","0")) != 0) {
+        } else {
             log.debug("setAudioSpeed does nothing coz passthrough");
             mAudioSpeed = 1.0f;
         }
