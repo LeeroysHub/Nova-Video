39ad33ea5b05a540cf5493605f766205f7e2e396
Courville Software <software@courville.org>
2025-11-09 19:03:41 +0100

    Reapply "add atempo filter enabling/disabling logic"
    This reverts commit a855b6d4a4206c1195ebb2ec2b715d9d069265c2.

--- src/main/java/org/leeroy/mediaplayer/video/player/PlayerActivity.java
+++ src/main/java/org/leeroy/mediaplayer/video/player/PlayerActivity.java
@@ -834,6 +834,7 @@ public class PlayerActivity extends AppCompatActivity implements PlayerControlle
             }
             LibAvos.setStreamMaxIframeSize(finalSize);
             LibAvos.enableAudioSpeed(mPreferences.getBoolean(KEY_PLAYBACK_SPEED,false));
+            LibAvos.disableAtempoFilter(mPreferences.getBoolean(VideoPreferencesCommon.KEY_AUDIO_SPEED_AUDIOTRACK, false));
             LibAvos.setAndroidFrameTiming(mPreferences.getBoolean(KEY_ENABLE_ANDROID_FRAME_TIMING,false));
             LibAvos.setRaisePriority(mPreferences.getBoolean(KEY_INCREASE_DECODER_PRIORITY,false));
             LibAvos.setAudioSpeed(audioSpeed); // set audio speed playback (does nothing if audio speed not enabled)
