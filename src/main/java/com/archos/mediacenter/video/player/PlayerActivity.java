4cc4ab06e4ee909154a2288af8223a441fc30140
Courville Software <software@courville.org>
2025-11-16 11:56:44 +0100

    Pass HDMI PCM channel capability to avos

--- src/main/java/com/archos/mediacenter/video/player/PlayerActivity.java
+++ src/main/java/com/archos/mediacenter/video/player/PlayerActivity.java
@@ -781,7 +781,8 @@ public class PlayerActivity extends AppCompatActivity implements PlayerControlle
         if (LibAvos.isAvailable()) {
             VideoPreferencesCommon.resetPassthroughPref(mPreferences); // note this resets the audio_speed if in passthrough to 1.0f in prefs
             // enable passthrough only if HDMI is connected and enabled in options
-            LibAvos.setPassthrough(CustomApplication.isPassthroughSupported() ? Integer.parseInt(mPreferences.getString("force_audio_passthrough_multiple","0") ) : 0);
+            LibAvos.setMaxPcmChannels(LeeroyFlixApp.getMaxAudioChannelCount());
+            LibAvos.setPassthrough(LeeroyFlixApp.isPassthroughSupported() ? Integer.parseInt(mPreferences.getString("force_audio_passthrough_multiple","0") ) : 0);
             if (mPreferences.getBoolean(VideoPreferencesCommon.KEY_FORCE_AUDIO_PASSTHROUGH, false)) {
                 LibAvos.setHdmiSupportedAudioCodecs(CustomApplication.allHdmiAudioCodecs);
             } else {
