ea3773ba77ef79853f13c4e4a67e164155482352
Courville Software <software@courville.org>
2025-12-07 09:55:42 +0100

    trakt: sync at app foreground if enabled

--- src/main/java/org/leeroy/mediaplayer/video/LeeroyFlixApp.java
+++ src/main/java/org/leeroy/mediaplayer/video/LeeroyFlixApp.java
@@ -48,8 +48,8 @@ import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ProcessLifecycleOwner;
 import androidx.preference.PreferenceManager;
 
-import org.leeroy.environment.LeeroyFlixFeatures;
-import org.leeroy.environment.LeeroyFlixUtils;
+import org.leeroy.environment.ArchosFeatures;
+import org.leeroy.environment.ArchosUtils;
 import org.leeroy.environment.NetworkState;
 import org.leeroy.filecorelibrary.FileUtilsQ;
 import org.leeroy.filecorelibrary.jcifs.JcifsUtils;
@@ -63,6 +63,7 @@ import org.leeroy.mediaplayer.utils.trakt.TraktService;
 import org.leeroy.mediaplayer.video.browser.BootupRecommandationService;
 import org.leeroy.mediaplayer.video.picasso.SmbRequestHandler;
 import org.leeroy.mediaplayer.video.picasso.ThumbnailRequestHandler;
+import org.leeroy.mediaplayer.video.player.PrivateMode;
 import org.leeroy.mediaplayer.video.player.PlayerActivity;
 import org.leeroy.mediaplayer.video.utils.LocaleConfigParser;
 import org.leeroy.mediaplayer.video.utils.OpenSubtitlesApiHelper;
@@ -424,8 +425,6 @@ public class LeeroyFlixApp extends Application implements DefaultLifecycleObserv
         }).start();
 
         Trakt.initApiKeys(this);
-        // Start TraktService to enable lifecycle callbacks and automatic syncing
-        TraktService.syncAtStart(this);
         new Thread() {
             public void run() {
                 this.setPriority(Thread.MIN_PRIORITY);
@@ -594,6 +593,10 @@ public class LeeroyFlixApp extends Application implements DefaultLifecycleObserv
             }
             addNetworkListener();
             launchSambaDiscovery();
+            // Trigger an incremental Trakt sync when returning to foreground if signed in and not in private mode
+            if (Trakt.isTraktV2Enabled(this, PreferenceManager.getDefaultSharedPreferences(this))) {
+                TraktService.sync(this, TraktService.FLAG_SYNC_AUTO);
+            }
         } else {
             unRegisterHdmiAudioPlugReceiver();
             unRegisterAudioDeviceCallback();
